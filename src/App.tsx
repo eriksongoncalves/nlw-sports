0fdd329e46b170c1614756a3d164ad301cb75145
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-13 11:31:57 -0300

    feat: styles and list games

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,87 @@
 import './styles/main.css'
 
+import logo from './assets/logo-nlw-sports.svg'
+
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-500">
-      <h1>NLW Sports</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="Logo NLW" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="bg-nlw-gradient bg-clip-text text-transparent">
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/games/game-1.jpg" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative">
+          <img src="/games/game-3.jpg" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative">
+          <img src="/games/game-4.jpg" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative">
+          <img src="/games/game-5.jpg" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative">
+          <img src="/games/game-4.jpg" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative">
+          <img src="/games/game-5.jpg" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/**.tsx'],
+  content: ['./src/**/**.tsx', './index.html'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        galaxy: "url('./public/background-galaxy.jpg')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC 27.08%, #43e7ad 33.94%, #e1d55d 40.57%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    }
   },
   plugins: []
 }
