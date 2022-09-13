76164d05e6f530b9fbb88cc0718069c6a0e35a08
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-13 10:20:41 -0300

    feat: config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import './styles/main.css'
+
 function App() {
-  return <h1>NLW Sports</h1>
+  return (
+    <div className="w-8 h-8 bg-violet-500">
+      <h1>NLW Sports</h1>
+    </div>
+  )
 }
 
 export { App }
