b8a3fa0fe223f79f53776eec97b9617e747c1b21
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-13 21:48:59 -0300

    feat: created a box dont found duo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css'
 
 import logo from './assets/logo-nlw-sports.svg'
@@ -81,6 +83,24 @@ function App() {
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#242634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou o seu duo?
+            </strong>
+            <span className="text-zinc-400">
+              Publique um anúncio para encontrar nos players!
+            </span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   )
