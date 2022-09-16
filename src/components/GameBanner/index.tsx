2fa3eeb048048eeb5e46ef0c12b00ae654b5e672
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-16 20:09:35 -0300

    feat: get games by api

--- src/components/GameBanner/index.tsx
+++ src/components/GameBanner/index.tsx
@@ -7,7 +7,7 @@ type GameBannerProps = {
 export const GameBanner = ({ bannerUrl, title, adsCount }: GameBannerProps) => {
   return (
     <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={bannerUrl} alt={title} />
+      <img src={bannerUrl} alt={title} className="w-full" />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
